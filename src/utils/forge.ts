6a4a3b9405bcdadc17554167dccf48e10155bf8e
Mark <mark@remarkablemark.org>
2024-12-20 01:00:24 -0500

    refactor(utils): replace fetch with forge sdk

--- src/utils/forge.ts
+++ src/utils/forge.ts
@@ -1,23 +1,13 @@
-import { Buffer } from 'buffer';
+import Forge from '@forgegames/sdk';
 
 const username = import.meta.env.VITE_FORGE_CLIENT_ID;
 const password = import.meta.env.VITE_FORGE_CLIENT_SECRET;
 
-const options = {
-  method: 'GET',
-  headers: {
-    Authorization: `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`,
-  },
-};
+const forge = new Forge({
+  username,
+  password,
+});
 
 export function getItems() {
-  return (
-    fetch(
-      'https://corsmirror.com/v1?url=https://forge-games.xyz/api/items',
-      options,
-    )
-      .then((response) => response.json())
-      // eslint-disable-next-line no-console
-      .catch((err) => console.error(err))
-  );
+  return forge.items.list();
 }
